be400bc1688b4c03e58801633e597bebae2e441b
Sankalp Boolchandani <boolchandanisankalp@gmail.com>
2024-12-09 19:47:07 +0530

    django admin changes

--- Hello/urls.py
+++ Hello/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "Porche Ind Admin"
+admin.site.site_title = "Porche Ind. Admin Portal"
+admin.site.index_title = "Welcome to Porche Ind"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
