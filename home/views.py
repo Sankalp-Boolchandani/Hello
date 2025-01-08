521dcc2064e75ff06ab01d5207b12455baece40e
Sankalp Boolchandani <boolchandanisankalp@gmail.com>
2025-01-08 20:17:43 +0530

    08Jan25 - Migrations and first db operation, added a contact

--- home/views.py
+++ home/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, HttpResponse
+from home.models import Contact
+from datetime import datetime
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -14,5 +17,13 @@ def services(request):
     # return HttpResponse("this is services page")
 
 def contact(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        phone = request.POST.get("phone")
+        desc = request.POST.get("desc")
+        contact = Contact(name=name, email=email, phone=phone, desc=desc, date=datetime.today())
+        contact.save()
+        messages.success(request, "Profile details updated.")
     return render(request, "contacts.html")
     # return HttpResponse("this is contacts page")
